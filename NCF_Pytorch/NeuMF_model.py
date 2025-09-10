303c5bc1606c1734bf5b62e2ce2d46989fd34fda
panchaldhruv27223 <panchaldhruv27223@gmail.com>
2025-09-10 23:54:26 +0530

    NeuMF Done -Code By Dhruv Panchal

--- NCF_Pytorch/NeuMF_model.py
+++ NCF_Pytorch/NeuMF_model.py
@@ -3,41 +3,85 @@ import torch.nn as nn
 import torch.optim as optim
 import os, sys, time
 
-class GMF(nn.Module):
-    def __init__(self, num_users, num_items, latent_dim, reg):
-        super(GMF, self).__init__()
+class NeuMF(nn.Module):
+    """
+    NeuMF: Neural Matrix Factorization
+    Combines GMF (element-wise product) and MLP (concatenation + feed-forward).
+    """
+    def __init__(self, num_users, num_items, latent_dim, layers=[32,16,8]):
+        super(NeuMF, self).__init__()
+        assert len(layers) >= 2
+        assert layers[0]%2 == 0
         
-        ## Users and items embeddings
+        # ---------- GMF embeddings ----------
+        self.user_embeddings_mf = nn.Embedding(num_embeddings=num_users, embedding_dim=latent_dim)
+        self.item_embeddings_mf = nn.Embedding(num_embeddings=num_items, embedding_dim=latent_dim)
+
+        # ---------- MLP embeddings ----------
+        self.user_embeddings_mlp = nn.Embedding(num_embeddings=num_users, embedding_dim=layers[0]//2)
+        self.item_embeddings_mlp = nn.Embedding(num_embeddings=num_items, embedding_dim=layers[0]//2)
         
-        self.user_embeddings = nn.Embedding(num_embeddings=num_users, embedding_dim=latent_dim)
-        self.item_embeddings = nn.Embedding(num_embeddings=num_items, embedding_dim=latent_dim)
-        
-        self.output = nn.Linear(latent_dim, 1)
+        # ---------- MLP hidden layers ----------
+        mlp_module = []
+        for input, output in zip(layers[:-1], layers[1:]):
+            mlp_module.append(nn.Linear(input, output))
+            mlp_module.append(nn.ReLU())    
+        self.mlp = nn.Sequential(*mlp_module)
+        ## now lets create final layer 
+        ## it take input of both MF and MLP
+        final_dim = latent_dim + layers[-1]
+        self.predict_layer = nn.Linear(final_dim, 1)
         self.sigmoid = nn.Sigmoid()
+        self._init_weights()
         
-        ### set the initializer 
-        nn.init.normal_(self.user_embeddings.weight, mean=0, std = 0.01)
-        nn.init.normal_(self.item_embeddings.weight, mean=0, std = 0.01)
+    def _init_weights(self):
+        """Initialize embeddings and linear layers"""
+        nn.init.normal_(self.item_embeddings_mf.weight, std=0.01)
+        nn.init.normal_(self.user_embeddings_mf.weight, std=0.01)
+        nn.init.normal_(self.user_embeddings_mlp.weight, std=0.01)
+        nn.init.normal_(self.item_embeddings_mlp.weight, std=0.01)
         
+        for m in self.mlp:
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+                    
+        nn.init.xavier_uniform_(self.predict_layer.weight)
+        if self.predict_layer.bias is not None:
+            nn.init.zeros_(self.predict_layer.bias)
         
     def forward(self, user, item):
-        user_latent = self.user_embeddings(user)
-        item_latent = self.item_embeddings(item)
-        
-        elementwise_product  = user_latent * item_latent
-        
-        out = self.output(elementwise_product )
-        logits = self.sigmoid(out)
-        logits = logits.squeeze()
-        # print(logits)
-        return logits 
-    
+        """
+        Args:
+            user
+            item
+        Returns:
+            probs: Tensor of shape (batch,), probabilities after sigmoid
+        """
 
-def train_GMF_model(model, train_loader, test_negative_dataset, config, NCFEvaluation, device="cpu"):
+        ## GMF Branch 
+        mf_u = self.user_embeddings_mf(user)
+        mf_i = self.item_embeddings_mf(item)
+        mf_point_wise_multiplication = mf_u*mf_i
+        
+        ## MLP Branch
+        mlp_u = self.user_embeddings_mlp(user)
+        mlp_i = self.item_embeddings_mlp(item)
+        mlp_input = torch.concat([mlp_u, mlp_i], dim=-1)
+        mlp_output = self.mlp(mlp_input)
+        
+        ## Now concat both MF and MLP output
+        merged_input = torch.concat([mf_point_wise_multiplication, mlp_output], dim=-1)
+        logits = self.predict_layer(merged_input)
+        out_prob = self.sigmoid(logits)
+        out_prob = out_prob.squeeze()
+        return out_prob
+
+def train_NeuMF_model(model, train_loader, test_negative_dataset, config, NCFEvaluation, device="cpu"):
     
     if config["learner"].lower() == "adam":
         optimizer = optim.Adam(model.parameters(), lr=config["lr"])
-        
     elif config["learner"].lower() == "adagrad":
         optimizer = optim.Adagrad(model.parameters(), lr=config["lr"])
     elif config["learner"].lower() == "rmsprop":
@@ -46,16 +90,11 @@ def train_GMF_model(model, train_loader, test_negative_dataset, config, NCFEvalu
         optimizer = optim.SGD(model.parameters(), lr=config["lr"])
         
     criterion = nn.BCELoss()
-    
     evaluator = NCFEvaluation(model, test_negative_dataset, top_k = config["topK"])
-    
     best_hr, best_ndcg, best_epoch = 0, 0, -1
-    
     for epoch in range(config["epochs"]):
         t1 = time.time()
-        
         model.train()
-        
         total_loss = 0 
         
         for users, items, labels in train_loader:
@@ -64,19 +103,10 @@ def train_GMF_model(model, train_loader, test_negative_dataset, config, NCFEvalu
             labels = labels.to(device)
             
             optimizer.zero_grad()
-            
             outputs = model(users, items)
-            
-            ## making the input and output shape and datatype same
-            
             outputs = outputs.to(torch.float32)
             labels = labels.to(torch.float32)
-            
-            # print(labels.shape)
-            # print(outputs.shape)
-            # print(labels.dtype)
-            # print(outputs.dtype)
-            # print(outputs)
+
             loss = criterion(outputs, labels)
             
             loss.backward()
@@ -99,13 +129,44 @@ def train_GMF_model(model, train_loader, test_negative_dataset, config, NCFEvalu
             
             if config["out"]:
                 torch.save(model.state_dict(),
-                           f"{config["dataset"]}_GMF_{config["num_factors"]}.pth")
+                           f"{config["dataset"]}_NeuMF_{config["num_factors"]}.pth")
             print(f"End. Best Iteration {epoch}: HR: {best_hr:.4f}, NDCG:{best_ndcg:.4f}")
             
-    print(f"The Best GMF Model is Saved from epoch {best_epoch}")
+    print(f"The Best NeuMF Model is Saved from epoch {best_epoch}")
     
     return best_hr, best_ndcg
-    
-    
+
 if __name__ == "__main__":
-    print("Calling From GMF Model.")
+    print("Calling From NeuMF Model.")
+    
+## next try to use pretrained 
+    # def load_pretrained(self, gm_model=None, mlp_model=None, strict: bool = True):
+    #     """
+    #     Load pretrained GMF/MLP weights into NeuMF.
+    #     gm_model: GMF instance with user_embedding, item_embedding
+    #     mlp_model: MLP instance with user_embedding, item_embedding, mlp layers
+    #     """
+    #     if gm_model is not None:
+    #         try:
+    #             self.mf_user_emb.weight.data.copy_(gm_model.user_embedding.weight.data)
+    #             self.mf_item_emb.weight.data.copy_(gm_model.item_embedding.weight.data)
+    #         except Exception as e:
+    #             if strict:
+    #                 raise RuntimeError(f"GMF weight loading failed: {e}")
+
+    #     if mlp_model is not None:
+    #         try:
+    #             self.mlp_user_emb.weight.data.copy_(mlp_model.user_embedding.weight.data)
+    #             self.mlp_item_emb.weight.data.copy_(mlp_model.item_embedding.weight.data)
+
+    #             # copy over linear layers (if shapes match)
+    #             mlp_layers_src = [m for m in mlp_model.mlp if isinstance(m, nn.Linear)]
+    #             mlp_layers_dst = [m for m in self.mlp if isinstance(m, nn.Linear)]
+    #             for src, dst in zip(mlp_layers_src, mlp_layers_dst):
+    #                 if src.weight.shape == dst.weight.shape:
+    #                     dst.weight.data.copy_(src.weight.data)
+    #                     if src.bias is not None and dst.bias is not None:
+    #                         dst.bias.data.copy_(src.bias.data)
+    #         except Exception as e:
+    #             if strict:
+    #                 raise RuntimeError(f"MLP weight loading failed: {e}")
--- NCF_Pytorch/NeuMF_model_train.py
+++ NCF_Pytorch/NeuMF_model_train.py
@@ -2,7 +2,7 @@ import os, sys, time
 import torch
 from torch.utils.data import DataLoader
 
-from GMF_model import GMF, train_GMF_model
+from NeuMF_model import NeuMF, train_NeuMF_model
 from ml_1m_dataset import NCFTrainDataset, NCFTestDataset
 from NCF_evaluation import NCFEvaluator
 
@@ -14,6 +14,7 @@ configurations = {
     "test_negative_data" : r"NCF_Pytorch\test_negative_data.csv",
     'dataset': 'ml-1m',   ## name of dataset
     'regs': [0, 0],       ## Regularigaion L1, L2
+    'layers': [32, 16,8],
     'lr': 0.001,          ## Learning Rate
     'batch_size': 256,    ## Batch Size
     'epochs': 3,          ## Training Epochs
@@ -21,7 +22,7 @@ configurations = {
     'num_factors': 10,    ## we used it as latent Dimensions
     'num_neg': 2,         ## per User no of negative items
     'out': True,          ## Save best model or not
-    "topK": 10            ## Used in Evaluation.
+    'topK': 10            ## Used in Evaluation.
 }
 
 
@@ -36,16 +37,16 @@ test_data_object = NCFTestDataset(test_csv=configurations["test_data"], test_neg
 num_users = train_data_object.num_users
 num_items = train_data_object.num_items
 
-Model = GMF(num_users=num_users, num_items=num_items, latent_dim=configurations["num_factors"], reg=configurations["regs"])
+Model = NeuMF(num_users=num_users, num_items=num_items, latent_dim=configurations["num_factors"], layers=[32,16,8])
 
 train_data_loader = DataLoader(train_data_object, configurations["batch_size"], shuffle=False)
 test_data_loader = DataLoader(test_data_object, configurations["batch_size"], shuffle=False)
 
-## Finally Train GMF Model
+## Finally Train NeuMF Model
 
-train_GMF_model(Model, train_loader=train_data_loader, test_negative_dataset=test_data_object, config=configurations, NCFEvaluation=NCFEvaluator)  
+train_NeuMF_model(Model, train_loader=train_data_loader, test_negative_dataset=test_data_object, config=configurations, NCFEvaluation=NCFEvaluator)  
 
 
 
 if __name__ == "__main__":
-    print("Calling from GMF Training.")
+    print("Calling from NeuMF Training.")
